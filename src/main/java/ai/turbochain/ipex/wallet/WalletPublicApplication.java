48bf4de4b0e585f7eadab0d0f2b538dbb318d569
zhangmingcheng <zhangmingcheng@pansoft.com>
2019-12-26 08:48:12 +0800

    开启定时器

--- src/main/java/ai/turbochain/ipex/wallet/WalletPublicApplication.java
+++ src/main/java/ai/turbochain/ipex/wallet/WalletPublicApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+/**
+ * 
+ * @author zmc
+ *
+ */
 @EnableEurekaClient
 @SpringBootApplication
 @EnableScheduling
