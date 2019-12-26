35cd10d5551392c1b4407a7166059f9a3185376c
zhangmingcheng <zhangmingcheng@pansoft.com>
2019-12-26 08:44:02 +0800

    开启定时器

--- src/main/java/ai/turbochain/ipex/wallet/WalletPublicApplication.java
+++ src/main/java/ai/turbochain/ipex/wallet/WalletPublicApplication.java
@@ -3,9 +3,11 @@ package ai.turbochain.ipex.wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableScheduling
 public class WalletPublicApplication {
     public static void main(String[] args){
         SpringApplication.run(WalletPublicApplication.class,args);
